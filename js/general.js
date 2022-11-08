b4d77f92e71b615190da8c4419320f67f863bd1b
Rakultur <76803753+Rakultur@users.noreply.github.com>
2022-11-08 15:19:12 -0500

    Se modifica Js

--- js/general.js
+++ js/general.js
@@ -1,7 +1,33 @@
 // Menu responsive
-const burgerIcon = document.querySelector('#navbarMenu');
-const navbarMenu = document.querySelector('#navbar-item');
+document.addEventListener('DOMContentLoaded', () => {
 
-burgerIcon.addEventListener('click', () =>{
-    navbarMenu.classList.toggle('is-active')
-})
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach(el => {
+    el.addEventListener('click', () => {
+
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+  // JS Para zoom imagenes
+  $(document).ready(function () {
+    $('.zoom').hover(function () {
+      $(this).addClass('transition');
+    }, function () {
+      $(this).removeClass('transition');
+    });
+  });
+
+});
+
+
