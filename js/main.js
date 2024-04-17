b4c7c7105f61ecb8e4fd8f90f9c43ec0864dc923
Nicolas Garcia <Rakultur@hotmail.com>
2024-04-17 14:26:26 -0500

    Modificaciones

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 
 // Interruptor
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
-            function cambiaTema(ev){
-                if(ev.target.checked){
-                    document.documentElement.setAttribute('tema', 'light');
-                } else {
-                    document.documentElement.setAttribute('tema', 'dark');
-                }
-            }
-            colorSwitch.addEventListener('change', cambiaTema);
+function cambiaTema(ev) {
+    if (ev.target.checked) {
+        document.documentElement.setAttribute('tema', 'claro');
+    } else {
+        document.documentElement.setAttribute('tema', 'oscuro');
+    }
+}
+colorSwitch.addEventListener('change', cambiaTema);
 
