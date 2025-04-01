47b7bdf151bc7921ccf2d9183c66b6d83d89b9b5
Nicolas Garcia <Rakultur@hotmail.com>
2025-04-01 16:57:00 -0500

    Modificaciones
    Se agregan modificaciones generales a la propuesta de web básica del apartado de proyectos Front

--- proyectos/front end/sitio web basica/js/propusta.js
+++ proyectos/front end/sitio web basica/js/propusta.js
@@ -1,1 +1,16 @@
 // Propuesta JavaScrip
+
+// Burger
+const nav = document.querySelector("#nav");
+const abrir = document.querySelector("#abrir");
+const cerrar = document.querySelector("#cerrar");
+
+abrir.addEventListener("click", () => {
+    nav.classList.add("visible");
+    body.classList.add("scroll");
+});
+
+cerrar.addEventListener("click", () => {
+    nav.classList.remove("visible");
+    body.classList.remove("scroll");
+});
