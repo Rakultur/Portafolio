3c1287b4eac0cb29b8d8d600d4b7a0716d48cddb
Nicolas Garcia <Rakultur@hotmail.com>
2025-04-19 22:19:17 -0500

    Modificaciones

--- javascript/rkt.js
+++ javascript/rkt.js
@@ -1,6 +1,28 @@
 // rkt - JavaScript
 
-// Interruptor
+// Modal
+
+var modal = document.getElementById("modal");
+
+var btn = document.getElementById("btn-modal");
+
+var span = document.getElementsByClassName("modal-btn-cerrar")[0];
+
+btn.onclick = function () {
+    modal.style.display = "block";
+}
+
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Interruptor cambio de tema
 const colorSwitch = document.querySelector('#switch input[type="checkbox"]');
 function cambiaTema(ev) {
     if (ev.target.checked) {
@@ -17,3 +39,4 @@ colorSwitch.addEventListener('change', cambiaTema);
 function MensajeMantenimiento() {
     alert("¡Actualmente se encuentra esta app en mantenimiento!");
 }
+
